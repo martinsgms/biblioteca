e86cfeb5d03368c175455cf44f639aeb06b71500
Gabriel Martins <gabriel.santos@egs.com.br>
2020-06-14 23:03:08 -0300

    configura acessos e permissões

--- src/main/java/br/com/martins/biblioteca/conf/SecurityConfiguration.java
+++ src/main/java/br/com/martins/biblioteca/conf/SecurityConfiguration.java
@@ -1,10 +1,27 @@
 package br.com.martins.biblioteca.conf;
 
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableWebSecurity
 public class SecurityConfiguration 
     extends WebSecurityConfigurerAdapter {
+    
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+        .antMatchers("/resources/**").permitAll()
+        .antMatchers("/carrinho/**").permitAll()
+        .antMatchers("/pagamento/**").permitAll()
+        .antMatchers("/produto/form").hasRole("ADMIN")
+        .antMatchers(HttpMethod.POST, "/produto").hasRole("ADMIN")
+        .antMatchers(HttpMethod.GET, "/produto").permitAll()
+        .antMatchers("/produto/**").permitAll()
+        .antMatchers("/").permitAll()
+        .anyRequest().authenticated()
+        .and().formLogin();
+    }
 
 }
